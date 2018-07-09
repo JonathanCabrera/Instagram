f8b1e31ff3ce1b1a062dcdf500f50e0eeeebbb0d
Jonathan Cabrera <noj@fb.com>
2018-07-09 15:10:52 -0700

    Initial Commit

---
+++ FeasoInstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FeasoInstagram
+//
+//  Created by Jonathan Cabrera on 7/9/18.
+//  Copyright © 2018 Jonathan Cabrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FeasoInstagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FeasoInstagram
+//
+//  Created by Jonathan Cabrera on 7/9/18.
+//  Copyright © 2018 Jonathan Cabrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
